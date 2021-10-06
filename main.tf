4e192da357979cb892cb79c83d0033a1451421d6
Athanasios Garyfalos <athanasios.garyfalos@maersk.com>
2021-10-06 15:20:41 +0200

    main Adding first commit with working part of code.

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "docker_container" "hello_world_app" {
+  image = "hello_world_app:latest"
+  name  = "hello_world_app"
+  restart = "always"
+  volumes {
+    container_path  = "/myapp"
+    # replace the host_path with full path for your project directory starting from root directory /
+    # host_path = abspath(path.root)
+    read_only = false
+  }
+  ports {
+    internal = 8080
+    external = 8080
+  }
+}
